357d31f9ef94f125a71155476039f9ea4539617f
Carlos Gochez <cgotcha@gmail.com>
2013-11-21 18:06:45 -0600

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorMusic::Application.config.secret_token = '49bdca566aec7263422bc07c676343874e74c6000037197ad2f691ef4289b51348619ff1659f0d6035f9d5bc5a579c5c334d41da19cb0d25c81cf8401a26cb05'
